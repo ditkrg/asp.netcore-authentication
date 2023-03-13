7ad90411ff1be5429926d0ad3fed3ee61754f66b
Shkar T. Noori <shkarface@gmail.com>
2023-03-14 00:38:19 +0300

    Change default section from Authentication:Gateway to Gateway

--- src/GatewayAuth/Defaults.cs
+++ src/GatewayAuth/Defaults.cs
@@ -1,13 +1,10 @@
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Http;
-
 namespace DIT.Authentication.GatewayAuth;
 
 public static class GatewayAuthDefaults
 {
     public const string AuthenticationScheme = "Gateway";
 
-    public const string ConfigurationSection = "Authentication:Gateway";
+    public const string ConfigurationSection = "Gateway";
 
     public const string UserHeader = "x-auth-user";
 
